a6c1a8b70a397d3b85ff4fe2f854e39a83e89258
Jason Schmitt <schmitty890@gmail.com>
2018-12-09 17:08:34 -0500

    handlebar template basic beginning structure setup

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,12 +1,15 @@
-var testdata = require('../data/testData.json');
+var indexData = require('../data/indexData.json');
+var gettingStartedData = require('../data/gettingStartedData.json');
+var guidelinesData = require('../data/guidelinesData.json');
 
 module.exports = function (app) {
   // Home Page
   app.get('/', function (req, res) {
     // assign the handlebar object any data to be read into the template. this separates the data from the markup.
     var hbsObject = {
-      testdata: testdata
+      pageData: indexData
     }
+    console.log(hbsObject.pageData.header);
     // console.log(hbsObject);
     res.render('index', {
       title: 'Home', // pass any value to handlebar template
@@ -14,14 +17,29 @@ module.exports = function (app) {
     });
   });
 
-  // Home Page
+  // Getting Started Page
   app.get('/getting-started', function (req, res) {
     // assign the handlebar object any data to be read into the template. this separates the data from the markup.
     var hbsObject = {
-      testdata: testdata
+      pageData: gettingStartedData
     }
+    console.log(hbsObject.pageData.header);
     // console.log(hbsObject);
-    res.render('index', {
+    res.render('partials/pages/getting-started', {
+      title: 'Home', // pass any value to handlebar template
+      hbsObject: hbsObject
+    });
+  });
+
+  // Guidelines Overview Page
+  app.get('/guidelines', function (req, res) {
+    // assign the handlebar object any data to be read into the template. this separates the data from the markup.
+    var hbsObject = {
+      pageData: guidelinesData
+    }
+    console.log(hbsObject.pageData.header);
+    // console.log(hbsObject);
+    res.render('partials/pages/getting-started', {
       title: 'Home', // pass any value to handlebar template
       hbsObject: hbsObject
     });
