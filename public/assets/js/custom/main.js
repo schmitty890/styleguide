810e270b77d50adc92dcbbeb2ea2858f4f524a4c
jschmitt <schmitty890@gmail.com>
2019-03-09 17:07:20 -0500

    level up

--- public/assets/js/custom/main.js
+++ public/assets/js/custom/main.js
@@ -36,8 +36,7 @@ $(function() {
   var AddIsActiveClassToNavigation = (function() {
     const checkURL = function() {
       var path = location.pathname.split('/')[1];
-      console.log(path);
-      $('.c-header__nav-container').find(`[data-attr='${path}']`).css('border', '1px solid red');
+      $('.c-header__nav-container').find(`[data-attr='${path}']`).addClass('is-active');
     };
 
     // this init function initializes the functions to be executed
@@ -55,56 +54,38 @@ $(function() {
 
 
   
+  $('.styleguide-code').each(function( index ) {
+    // console.log( index + ": " + $( this ).html() );
+    var code = $(this).html();
+    // $(this).siblings('.code-snippet').css('border', '5px solid red');
+    var html = Prism.highlight(code, Prism.languages.html);
+    // $(this).siblings('.code-to-copy').append(html);
+    $(this).next('.code-snippet').find('.code-to-copy').append(html);
+  });
 
+  // new Clipboard('#test');
+  var clipboard = new ClipboardJS('.copy', {
+    target: function(trigger) {
+      return trigger.previousElementSibling;
+    }
+  });
+  clipboard.on('success', function(e) {
+      console.info('Action:', e.action);
+      console.info('Text:', e.text);
+      console.info('Trigger:', e.trigger);
+
+      e.clearSelection();
+  });
+  clipboard.on('error', function(e) {
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+  });
 
 
 
 });
 
-$('.styleguide-code').each(function( index ) {
-      // console.log( index + ": " + $( this ).html() );
-      var code = $(this).html();
-      // $(this).siblings('.code-snippet').css('border', '5px solid red');
-      var html = Prism.highlight(code, Prism.languages.html);
-      // $(this).siblings('.code-to-copy').append(html);
-      $(this).next('.code-snippet').find('.code-to-copy').append(html);
-    });
 
-    // console.log($code);
-    // $code.forEach(function( element ) {
-    //   // $(this).css('border', '5px solid red');
-    // });
-
-    // console.log(code);
-
-
-    // $('.target').append(html);
-
-    // var html = Prism.highlight(code, Prism.languages.html);
-
-
-    // console.log(html);
-    // $('.code-to-copy').append(html);
-
-
-
-    // new Clipboard('#test');
-    var clipboard = new ClipboardJS('.copy', {
-      target: function(trigger) {
-        return trigger.previousElementSibling;
-      }
-    });
-    clipboard.on('success', function(e) {
-        console.info('Action:', e.action);
-        console.info('Text:', e.text);
-        console.info('Trigger:', e.trigger);
-
-        e.clearSelection();
-    });
-    clipboard.on('error', function(e) {
-        console.error('Action:', e.action);
-        console.error('Trigger:', e.trigger);
-    });
 
 
 
